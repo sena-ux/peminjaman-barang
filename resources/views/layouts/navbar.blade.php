4024ad52706dbf8a3c133735e97ee3fb6e7827a3
sena-ux <119649820+sena-ux@users.noreply.github.com>
2024-09-09 08:51:41 +0800

    9/9/2024 Update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-primary fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Peminjaman Barang</a>
+        <a class="navbar-brand" href="{{ route('home')}}">Peminjaman Barang</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home')}}">Home</a>
                 </li>
             </ul>
         </div>
